28166b3585ab4586927978ef8ad1663235dc03ba
over-loadcode <overloadcode@gmail.com>
2024-06-05 16:10:12 -0400

    asdad

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ img.src = "bald.jpg";
 container.onload = () => {
   resizeCanvas();
   window.addEventListener("resize", resizeCanvas);
+  console.log('resized');
 };
 
 const resizeCanvas = () => {
