1f1739932169ecccb834c9e1e47bdf7f987acb89
Rohit Dasgupta <rdg922@gmail.com>
2024-06-05 01:15:43 -0400

    remove stretching

--- index.js
+++ index.js
@@ -2,38 +2,44 @@ const canvas = document.getElementById("drawing-board");
 const toolbar = document.getElementById("toolbar");
 const ctx = canvas.getContext("2d");
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
-
 let isPainting = false;
 let lineWidth = 5;
 let startX;
 let startY;
+let img = new Image();
+
+img.src = "bald.jpg";
+img.onload = () => {
+  resizeCanvas();
+  window.addEventListener("resize", resizeCanvas);
+};
+
+const resizeCanvas = () => {
+  const aspectRatio = img.width / img.height;
+  if (window.innerWidth / window.innerHeight > aspectRatio) {
+    canvas.height = window.innerHeight - toolbar.offsetHeight;
+    canvas.width = canvas.height * aspectRatio;
+  } else {
+    canvas.width = window.innerWidth;
+    canvas.height = canvas.width / aspectRatio;
+  }
+  canvas.style.marginTop = `${toolbar.offsetHeight}px`;
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
 
 toolbar.addEventListener("click", (e) => {
   if (e.target.id === "clear") {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    loadBackgroundImage();
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   } else if (e.target.id === "download") {
     downloadCanvas();
   }
 });
 
-const loadBackgroundImage = () => {
-  const img = new Image();
-  img.src = "bald.jpg";
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-  };
-};
-
 const startPosition = (e) => {
   isPainting = true;
-  startX = (e.clientX || e.touches[0].clientX) - canvasOffsetX;
-  startY = (e.clientY || e.touches[0].clientY) - canvasOffsetY;
+  startX = (e.clientX || e.touches[0].clientX) - canvas.offsetLeft;
+  startY = (e.clientY || e.touches[0].clientY) - canvas.offsetTop;
 };
 
 const finishedPosition = () => {
@@ -49,14 +55,14 @@ const draw = (e) => {
   ctx.lineCap = "round";
 
   ctx.lineTo(
-    (e.clientX || e.touches[0].clientX) - canvasOffsetX,
-    (e.clientY || e.touches[0].clientY) - canvasOffsetY,
+    (e.clientX || e.touches[0].clientX) - canvas.offsetLeft,
+    (e.clientY || e.touches[0].clientY) - canvas.offsetTop,
   );
   ctx.stroke();
   ctx.beginPath();
   ctx.moveTo(
-    (e.clientX || e.touches[0].clientX) - canvasOffsetX,
-    (e.clientY || e.touches[0].clientY) - canvasOffsetY,
+    (e.clientX || e.touches[0].clientX) - canvas.offsetLeft,
+    (e.clientY || e.touches[0].clientY) - canvas.offsetTop,
   );
 };
 
@@ -74,5 +80,3 @@ canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("touchstart", startPosition);
 canvas.addEventListener("touchend", finishedPosition);
 canvas.addEventListener("touchmove", draw);
-
-loadBackgroundImage();
